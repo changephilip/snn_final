a13f936392a4470974346e13afd619fd3e8a9ac5
changephilip <pcchange90@outlook.com>
2023-01-23 22:55:23 +0800

    Update hopfield

--- hodgkinHuxley/hhHopf.py
+++ hodgkinHuxley/hhHopf.py
@@ -9,36 +9,44 @@ V, m, h, n = sym.symbols('V,m,h,n')
 t = sym.Symbol('t')
 
 Cm = 1
-gNa = 120
-gK = 36
+gNa = 120.0
+gK = 36.0
 gL = 0.3
-VNa = 115
-VK = -12
+VNa = 115.0
+VK = -12.0
 VL = 10.599
 
 I = 120
 
 T = 3 ** (30)
 
-am = 0.1*(2.0 - V) / (sym.exp((25.0 - V)/10.0)-10.0)
+am = 0.1*(25.0 - V) / (sym.exp((25.0 - V)/10.0)-1.0)
 bm = 4.0 * sym.exp(-1.0 * V / 18.0)
 ah = 0.07 * sym.exp(-1.0 * V / 20.0)
-bh = 1.0 / sym.exp(((30 - V)/10.0) + 1.0)
+bh = 1.0 / (sym.exp((30 - V)/10.0) + 1.0)
 an = 0.01 * (10.0 - V) / (sym.exp((10.0 - V)/10.0) - 1.0)
 bn = 0.125 * sym.exp(-1.0 * V / 80.0)
 
 
 eqs = []
 
-eq1 = (I - gNa * (m**3) * h * (V - VNa) -
+eq1 = (- gNa * (m**3) * h * (V - VNa) -
        gK * (n**4)*(V - VK) - gL*(V - VL))
 
-eqm = sym.Eq(am * (1 - m) - bm * m)
+eq2 = sym.Eq(am * (1.0 - m) - bm * m)
 
-eqh = sym.Eq(ah * (1 - h) - bh * h)
+eq3 = sym.Eq(ah * (1.0 - h) - bh * h)
 
-eqn = sym.Eq(an * (1 - n) - bn * n)
+eq4 = sym.Eq(an * (1.0 - n) - bn * n)
 
-eq1.replace
+eqm = sym.solve(eq2,m)[0]
+eqh = sym.solve(eq3,h)[0]
+eqn = sym.solve(eq4,n)[0]
 
+eqFinal=eq1.replace(m,eqm).replace(h,eqh).replace(n,eqn)
 
+from sympy.plotting import plot
+
+plot(eqFinal,(V,-100,0))
+
+deq1= sym.Eq(sym.diff(V))
--- hopfield/hopfield.py
+++ hopfield/hopfield.py
@@ -6,45 +6,82 @@ import matplotlib.pyplot as plt
 class HopfieldTSP():
     def __init__(self,cities):
         self.cities = cities
-        self.nCitites = cities.shape[0]
+        self.nCities = cities.shape[0]
         self.u0 = 0.02
         self.deltaT = 1e-4
         self.A = 200
         self.D =100
-        self.W = np.zeros((self.n,self.n))
-        self.U = np.zeros((self.n,self.n))
-        self.V = np.zerors((self.n,self.n))
+        self.W = np.zeros((self.nCities,self.nCities))
+        self.U = np.zeros((self.nCities,self.nCities))
+        self.V = np.zeros((self.nCities,self.nCities))
         self.initWeight()
         self.initNode()
 
 #初始化权重矩阵
     def initWeight(self,):
-        for i in range(self.nCitites):
-            for j in range(self.nCitites):
-                self.W[i,j] = np.sqrt(np.sum((self.cities[i] - self.citites[j])**2))
+        for i in range(self.nCities):
+            for j in range(self.nCities):
+                self.W[i,j] = np.sqrt(np.sum((self.cities[i] - self.cities[j])**2))
 
     def initNode(self):
-        for i in range(self.nCitites):
-            for j in range(self.n):
-                self.U[i,j] = 0.5 * self.u0 * np.log(self.nCitites - 1) + np.random.random()
+        for i in range(self.nCities):
+            for j in range(self.nCities):
+                self.U[i,j] = 0.5 * self.u0 * np.log(self.nCities - 1) + np.random.random()*2 -1
 
     def getEnergy(self):
         energy = 0.0
 #逐次累加改进后的能量函数
-        for i in range(self.nCitites):
+        for i in range(self.nCities):
             #约束每行只有1个1
-            energy += 0.5 * self.A * (np.sum(self.V[i, :] - 1))**2 
+            energy += 0.5 * self.A * (np.sum(self.V[i, :]) - 1)**2 
             #约束每列只有1个1
-            energy += 0.5 * self.A * (np.sum(self.V[i:,1] - 1))**2
+            energy += 0.5 * self.A * (np.sum(self.V[:,i]) - 1)**2
             #路程
-            for j in range(self.n):
-                for k in range(self.n):
-                    t = k + 1 if k + 1 < self.nCitites else 0
+            for j in range(self.nCities):
+                for k in range(self.nCities):
+                    t = k + 1 if k + 1 < self.nCities else 0
                     energy += 0.5 * self.D * self.W[i,j] * self.V[i,k] * self.V[j,t]
                 
         return energy
 
     def diff(self,i ,j):
-        t = j+1 if j+1 < self.nCitites else 0
-        
+        t = j+1 if j+1 < self.nCities else 0
+        return -self.A*(np.sum(self.V[i]) - 1) - self.A * (np.sum(self.V[:,j]) - 1) - self.D * self.W[i,:].dot(self.V[:,t])
+
+    def check(self):
+        pos= np.where(self.V<0.2,0,1)
+        flag  =True
+        if np.sum(pos) != self.nCities:
+            flag = False
+        for i in range(self.nCities):
+            if np.sum(pos[:,i])!=1:
+                flag = False
+            if np.sum(pos[i,:])!=1:
+                flag = False
+        return flag
+
+    def __call__(self):
+        running_energy = []
+        iter = 0
+        while not self.check():
+            iter += 1
+            for i in range(self.nCities):
+                for j in range(self.nCities):
+                    self.U[i,j] += self.deltaT * self.diff(i,j)
+
+            self.V = 0.5 * ( 1+ np.tanh(self.U/ self.u0))
+            energy = self.getEnergy()
+            running_energy.append(energy)
+        return running_energy, np.where(self.V<0.2,0,1), iter
+
+if __name__ == "__main__":
+    cities = np.array([[2,6],[2,4],[1,3],[4,6],[5,5],[4,4],[6,4],[3,2]])
+    solver = HopfieldTSP(cities)
+    energy, answer,iter = solver()
+    print(answer)
+    print(iter)
+    #print(energy)
+    plt.plot(energy)
+    plt.show()
+
        
